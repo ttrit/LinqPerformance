208fe217439ac1c693d04e4bba6b1fe9f2add033
Thinh Truong <thinh2it@gmail.com>
2021-01-19 22:11:26 +0700

    demo deferred execution and lazy evaluation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqPerformance
 {
@@ -6,7 +8,30 @@ namespace LinqPerformance
     {
         static void Main(string[] args)
         {
+            DeferredExecutionAndLazyEvaluation();
+
             Console.ReadKey();
+        }
+
+        static void DeferredExecutionAndLazyEvaluation()
+        {
+            IList<Student> names = new List<Student>()
+            {
+                new Student() { Name = "Nam", Age = 24 },
+                new Student() { Name = "Cong", Age = 30 },
+                new Student() { Name = "Chien", Age = 18 },
+                new Student() { Name = "Hai", Age = 20 }
+            };
+
+            var cLetterName = from n in names
+                                                  where n.Name.StartsWith('C')
+                                                  select n;
+
+            foreach (var student in cLetterName)
+            {
+                Console.WriteLine(student.Name);
+            }
+
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqPerformance
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
